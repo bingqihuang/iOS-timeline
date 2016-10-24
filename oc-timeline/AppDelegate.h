f6b95c0945a91d48fa141aee92f2dbb4edf71413
bingqihuang <594301646@163.com>
2016-10-24 11:28:06 +0800

    Initial Commit

---
+++ oc-timeline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oc-timeline
+//
+//  Created by HuangBQ on 16/10/24.
+//  Copyright © 2016年 huangbingqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oc-timeline/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oc-timeline
+//
+//  Created by HuangBQ on 16/10/24.
+//  Copyright © 2016年 huangbingqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
